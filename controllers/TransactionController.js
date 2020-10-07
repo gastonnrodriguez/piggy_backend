93f03f078cb298656b555ef6b72b5f70fdc0341c
gastonnrodriguez <gastonnrodriguez@gmail.com>
2020-10-07 13:38:46 -0300

    CRUD Completed

--- controllers/TransactionController.js
+++ controllers/TransactionController.js
@@ -1,5 +1,5 @@
 const Transaction = require("../models/Transaction");
-
+const objectId = require('mongoose/lib/types/objectid');
 // @desc Get all transactions
 // @route GET /api/v1/transactions
 // @access Public
@@ -8,13 +8,11 @@ exports.getTransactions = async (req, res, next) => {
     const transactions = await Transaction.find();
 
     return res.status(200).json({
-      success: true,
       count: transactions.length,
       data: transactions,
     });
   } catch (error) {
     return res.status(500).json({
-      success: false,
       error: "server error yo!",
     });
   }
@@ -29,19 +27,16 @@ exports.addTransaction = async (req, res, next) => {
     const transaction = await Transaction.create(req.body);
 
     return res.status(201).json({
-      success: true,
       data: transaction,
     });
   } catch (error) {
     if (error.name === "ValidationError") {
       const messages = Object.values(error.errors).map((val) => val.message);
       return res.status(400).json({
-        success: false,
         error: messages,
       });
     } else {
       return res.status(500).json({
-        success: false,
         error: "server error yo!",
       });
     }
@@ -55,23 +50,25 @@ exports.updateTransaction = async (req, res, next) => {
   try {
     const { text, amount, category } = req.body;
     const transaction = await Transaction.findOneAndUpdate(req.params.id);
-    if (!transaction) {
-      return res.status(404).json({
-        success: false,
+    if (!transaction.ok) {
+      return res.status(400).json({
         error: "No transaction found",
       });
     }
-    
-      console.log(req.body);
-    await transaction.updateOne(req.body);
-    return res.status(200).json({
-      success: true,
-      data: transaction,
-    });
-  
+
+    const updateResponse = await transaction.updateOne(req.body);
+    if (!updateResponse.ok) {
+      return res.status(400).json({
+        error: "Error updating transaction",
+      });
+    } else {
+      return res.status(200).json({
+        before: transaction,
+        after: req.body,
+      });
+    }
   } catch (error) {
     return res.status(500).json({
-      success: false,
       error: "server error yo!",
     });
   }
@@ -83,21 +80,22 @@ exports.updateTransaction = async (req, res, next) => {
 exports.deleteTransaction = async (req, res, next) => {
   try {
     const transaction = await Transaction.findById(req.params.id);
-    if (!transaction) {
+
+    if (!objectId.isValid(req.params.id)) {
       return res.status(404).json({
         success: false,
         error: "No transaction found",
       });
     }
-
-    await transaction.remove();
-    return res.status(200).json({
-      success: true,
-      data: {},
-    });
+ else {
+      await transaction.remove();
+      return res.status(200).json({
+        transaction: transaction,
+        status: "successfully deleted",
+      });
+    }
   } catch (error) {
     return res.status(500).json({
-      success: false,
       error: "server error yo!",
     });
   }
--- server.js
+++ server.js
@@ -14,4 +14,4 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use('/api/v1/transactions', transactions);
 
-app.listen(PORT, console.log(`Server running on port ${PORT}`.yellow.bold));
+app.listen(PORT, console.log(`🐷 Server running on port ${PORT} 🐷`.yellow.bold));
