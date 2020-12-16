763f70067bfd2eab15b027555ec08b3c965951c8
gastonnrodriguez <gastonnrodriguez@gmail.com>
2020-12-15 23:10:59 -0300

    Enabling cors

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
+const cors = require('cors');
 const connectDB = require('./config/db');
 dotenv.config({path:'./config/config.env'});
 
@@ -12,6 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cors());
 app.use('/api/v1/transactions', transactions);
 
 app.listen(PORT, console.log(`🐷 Server running on port ${PORT} 🐷`.yellow.bold));
